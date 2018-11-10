26615a5edc5cf2839f995278c975b8c3438f23ca
pamelasanders <44900950+pamelasanders@users.noreply.github.com>
2018-11-10 06:00:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-     self.redirect("https://www.fatdecimator.com/", True)
+     self.redirect("https://www.mb103.com/lnk.asp?o=13719&c=918273&a=279700&l=14536", True)
 		# and this is the main code
 
 app = webapp2.WSGIApplication([
